f9f55a3c11bb23fdbb3cfb31d7bc5145331c31cb
robert <robert.k25@gmail.com>
2016-12-15 09:59:13 -0500

    adds controller actions, cors configuration, and routes for geocaches

--- geocache-api/app/controllers/geocaches_controller.rb
+++ geocache-api/app/controllers/geocaches_controller.rb
@@ -1,2 +1,42 @@
 class GeocachesController < ApplicationController
+  def index
+    @geocaches =  Geocache.all
+    render json: @geocaches
+  end
+
+  def show
+    @geocache =  Geocache.find(params[:id])
+    render json: @geocache
+  end
+
+  def update
+    @geocache = Geocache.find(params[:id])
+    if @geocache.update(geocache_params)
+      render json: @geocache
+    else
+      render json: @geocache.errors, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @geocache =  Geocache.new(geocache_params)
+    if @geocache.save
+      render json: @geocache
+    else
+      render json: @geocache.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @geocache =  Geocache.find(params[:id])
+    @geocache.destroy
+    @geocaches =  Geocache.all
+    render json: @geocaches
+  end
+
+  private
+
+  def geocache_params
+    params.require(:geocache).permit(:item, :long, :lat)
+  end
 end
