e8a8004d09513072f6403897af6e1d1d759a3eca
robert <robert.k25@gmail.com>
2016-12-15 10:14:33 -0500

    adds seeds file

--- geocache-api/db/seeds.rb
+++ geocache-api/db/seeds.rb
@@ -6,5 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+require 'faker'
 Geocache.destroy_all
+
+
+15.times do
+  Geocache.create(
+    item: Faker::Commerce.product_name,
+    long: Faker::Address.longitude,
+    lat: Faker::Address.latitude
+  )
+end
